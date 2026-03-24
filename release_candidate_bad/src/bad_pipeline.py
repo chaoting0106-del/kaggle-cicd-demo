324b0cb0be03aaf13237bf4a18b3e38a99fecbd1
chaoting0106-del <chaoting0106@gmail.com>
2026-03-24 22:09:13 +0800

    Update bad_pipeline.py
    Fix block

--- release_candidate_bad/src/bad_pipeline.py
+++ release_candidate_bad/src/bad_pipeline.py
@@ -1,8 +1,8 @@
 import os
 from pathlib import Path
 
-import pandas as pd
 import numpy as np
+import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
