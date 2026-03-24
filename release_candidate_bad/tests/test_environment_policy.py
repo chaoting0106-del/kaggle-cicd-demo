534fcad4f25ca0f5c4a3db55ad79f88dfcbba0de
chaoting0106-del <chaoting0106@gmail.com>
2026-03-24 23:43:55 +0800

    Update test_environment_policy.py

--- release_candidate_bad/tests/test_environment_policy.py
+++ release_candidate_bad/tests/test_environment_policy.py
@@ -17,7 +17,7 @@ def test_python_version_is_between_310_and_311() -> None:
     assert (3, 10) <= sys.version_info[:2] <= (3, 11)
 
 
-
 def test_library_versions_meet_policy() -> None:
     for package_name, min_version in MIN_VERSIONS.items():
         assert Version(version(package_name)) >= Version(min_version)
+
