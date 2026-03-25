5632c024946d053b4a2c41a4ffbe2f6d0c636b72
chaoting0106-del <chaoting0106@gmail.com>
2026-03-25 22:08:28 +0800

    Update bad_pipeline.py

--- release_candidate_bad/src/bad_pipeline.py
+++ release_candidate_bad/src/bad_pipeline.py
@@ -1,9 +1,10 @@
 import os
+
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split train_test_split
 
 # Standardised pathing
 LOCAL_FALLBACK_PATH = "C:/Users/dev/Desktop/titanic_train.csv"
